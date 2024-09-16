7bd54beb86b5fefdfa72d2d2df1bbc98f9eac900
Joe di Stefano <joeydi@okaypl.us>
2024-09-16 14:08:21 -0400

    add video and play/pause/hover functionality

--- app/components/Reel.tsx
+++ app/components/Reel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -10,17 +10,25 @@ import Container from "./Container";
 import ReelGrid from "./ReelGrid";
 import posterImage from "@/images/aston-martin-tach-ui.jpg";
 import playButton from "@/images/play-button.svg";
+import pauseButton from "@/images/pause-button.svg";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Reel = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const activityTimeoutRef = useRef(0);
   const sectionRef = useRef<HTMLElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const headingSpanRef = useRef<HTMLSpanElement>(null);
+  const playButtonWrapperRef = useRef<HTMLDivElement>(null);
   const playButtonRef = useRef<HTMLButtonElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
   const maskRef = useRef<HTMLDivElement>(null);
   const posterRef = useRef<HTMLImageElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useGSAP(() => {
     const headingTimeline = gsap.timeline({
@@ -128,6 +136,94 @@ const Reel = () => {
     );
   });
 
+  const mouseMoveHandler = (e: React.MouseEvent) => {
+    if (!playButtonWrapperRef.current) {
+      return;
+    }
+
+    const buttonRect = playButtonWrapperRef.current.getBoundingClientRect();
+    const x = e.clientX - buttonRect.x - buttonRect.width / 2;
+    const y = e.clientY - buttonRect.y - buttonRect.height / 2;
+
+    gsap.to(playButtonRef.current, {
+      x: x,
+      y: y,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+
+    gsap.set(playButtonRef.current, {
+      pointerEvents: "none",
+    });
+
+    setIsHovered(true);
+    setIsActive(true);
+
+    window.clearTimeout(activityTimeoutRef.current);
+    activityTimeoutRef.current = window.setTimeout(() => {
+      setIsActive(false);
+    }, 1000);
+  };
+
+  const mouseLeaveHandler = () => {
+    gsap.set(playButtonRef.current, {
+      pointerEvents: "all",
+    });
+
+    gsap.to(playButtonRef.current, {
+      x: 0,
+      y: 0,
+      duration: 1,
+      ease: "expo.out",
+    });
+
+    setIsHovered(false);
+  };
+
+  const clickHandler = () => {
+    setIsActive(true);
+    setIsPlaying(!isPlaying);
+
+    window.clearTimeout(activityTimeoutRef.current);
+    activityTimeoutRef.current = window.setTimeout(() => {
+      setIsActive(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      gsap.to(posterRef.current, {
+        opacity: 0,
+        duration: 0.25,
+      });
+      videoRef.current?.play();
+    } else {
+      gsap.to(posterRef.current, {
+        opacity: 1,
+        duration: 0.25,
+      });
+      videoRef.current?.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    if (isHovered && !isActive) {
+      gsap.to(playButtonRef.current, {
+        opacity: 0,
+        scale: 0.75,
+        duration: 0.25,
+        ease: "power2.in",
+      });
+    } else {
+      gsap.to(playButtonRef.current, {
+        opacity: 1,
+        scale: 1,
+        duration: 0.25,
+        ease: "power2.out",
+      });
+    }
+  }, [isHovered, isActive]);
+
   return (
     <section ref={sectionRef} className={styles.section}>
       <ReelGrid />
@@ -138,12 +234,20 @@ const Reel = () => {
               <h2>
                 <span ref={headingSpanRef}>UI Motion Design Reel</span>
               </h2>
-              <button ref={playButtonRef} className={styles.playButton}>
-                <Image src={playButton} alt="" />
-              </button>
+              <div ref={playButtonWrapperRef}>
+                <button ref={playButtonRef} className={styles.playButton} onClick={clickHandler}>
+                  <Image src={isPlaying ? pauseButton : playButton} alt="" />
+                </button>
+              </div>
             </div>
-            <div ref={wrapperRef} className={styles.wrapper}>
+            <div
+              ref={wrapperRef}
+              className={styles.wrapper}
+              onMouseMove={mouseMoveHandler}
+              onMouseLeave={mouseLeaveHandler}
+              onClick={clickHandler}>
               <div ref={maskRef} className={styles.mask}>
+                <video ref={videoRef} className={styles.video} src="https://simplecreature.us/assets/reelEdit_web.mp4" />
                 <Image
                   ref={posterRef}
                   className={styles.poster}
