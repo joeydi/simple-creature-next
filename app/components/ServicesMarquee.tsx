273e3a1b69e04712947a20e602eea570e3d22338
Joe di Stefano <joeydi@okaypl.us>
2024-09-18 14:56:48 -0400

    tweak ServicesMarquee parallax

--- app/components/ServicesMarquee.tsx
+++ app/components/ServicesMarquee.tsx
@@ -12,7 +12,6 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const ServicesMarquee = () => {
   const spacerRef = useRef<HTMLDivElement>(null);
-  const contentRef = useRef<HTMLDivElement>(null);
   const marqueeRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
@@ -25,7 +24,7 @@ const ServicesMarquee = () => {
         trigger: spacerRef.current,
         start: "top bottom",
         end: "bottom top",
-        scrub: 1,
+        scrub: true,
       },
     });
 
@@ -37,6 +36,7 @@ const ServicesMarquee = () => {
             const width = el.getBoundingClientRect().width;
             return window.innerWidth - width;
           },
+          duration: 1,
           ease: "power1.inOut",
         },
         0
@@ -51,35 +51,43 @@ const ServicesMarquee = () => {
             const width = el.getBoundingClientRect().width;
             return width - window.innerWidth;
           },
+          duration: 1,
           ease: "power1.inOut",
         },
         0
       );
     }
+
+    timeline.fromTo(
+      marqueeRef.current,
+      {
+        y: -window.innerHeight / 4,
+      },
+      { y: window.innerHeight / 4, duration: 1, ease: "none" },
+      0
+    );
   });
 
   return (
     <section>
+      <Container>
+        <h2 className={styles.heading}>
+          <MaskHeading reset={true}>Our Services</MaskHeading>
+        </h2>
+      </Container>
       <div ref={spacerRef} className={styles.spacer}>
-        <div ref={contentRef}>
-          <Container>
-            <h2 className={styles.heading}>
-              <MaskHeading>Our Services</MaskHeading>
-            </h2>
-          </Container>
-          <div ref={marqueeRef} className={`h1 ${styles.marquee}`} data-speed="0.5">
-            <span className={styles.span}>
-              Strategy &bull; Design &bull; Motion &bull; Consulting &bull; Interactive &bull; Production
-            </span>
-            <span className={styles.span}>Direction &bull; User Experience &bull; Branding &bull; Installations</span>
-            <span className={styles.span}>
-              3D Modeling &bull; Creative Direction &bull; Interface Design &bull; Creative Development
-            </span>
-            <span className={styles.span}>
-              Strategy &bull; Design &bull; Motion &bull; Consulting &bull; Interactive &bull; Production
-            </span>
-            <span className={styles.span}>Direction &bull; User Experience &bull; Branding &bull; Installations</span>
-          </div>
+        <div ref={marqueeRef} className={`h1 ${styles.marquee}`}>
+          <span className={styles.span}>
+            Strategy &bull; Design &bull; Motion &bull; Consulting &bull; Interactive &bull; Production
+          </span>
+          <span className={styles.span}>Direction &bull; User Experience &bull; Branding &bull; Installations</span>
+          <span className={styles.span}>
+            3D Modeling &bull; Creative Direction &bull; Interface Design &bull; Creative Development
+          </span>
+          <span className={styles.span}>
+            Strategy &bull; Design &bull; Motion &bull; Consulting &bull; Interactive &bull; Production
+          </span>
+          <span className={styles.span}>Direction &bull; User Experience &bull; Branding &bull; Installations</span>
         </div>
       </div>
     </section>
