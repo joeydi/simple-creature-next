fee8903f9827030f91dbb36923bccdf4ded4c7d9
Joe di Stefano <joeydi@okaypl.us>
2025-11-06 16:12:27 -0500

    make DashboardHeader sticky

--- app/components/dashboard-header.tsx
+++ app/components/dashboard-header.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react"
 
 export function DashboardHeader({ title, children }: { title: string; children?: ReactNode }) {
   return (
-    <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+    <header className="z-1 group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 sticky top-0 flex h-12 shrink-0 items-center gap-2 border-b bg-white transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 sm:px-6 lg:gap-2 lg:px-8">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
