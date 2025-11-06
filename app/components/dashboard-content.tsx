0b8a052cf514bd5acb4caad41e2a2ba975944952
Joe di Stefano <joeydi@okaypl.us>
2025-11-06 15:50:59 -0500

    use section for DashboardContent

--- app/components/dashboard-content.tsx
+++ app/components/dashboard-content.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils"
 import { ComponentPropsWithoutRef } from "react"
 
-export function DashboardContent({ className, ...props }: ComponentPropsWithoutRef<"div">) {
-  return <div className={cn("px-4 py-8 sm:px-6 lg:px-8", className)} {...props} />
+export function DashboardContent({ className, ...props }: ComponentPropsWithoutRef<"section">) {
+  return <section className={cn("px-4 py-8 sm:px-6 lg:px-8", className)} {...props} />
 }
