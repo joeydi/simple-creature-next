64cbba70926b2efd6fa5cbf4ac42e104a7157c88
Joe di Stefano <joeydi@okaypl.us>
2025-11-06 21:09:56 -0500

    ProjectCard animation updates

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -22,70 +22,106 @@ interface Props {
 
 const ProjectCard = ({ className = "", align = "left", slug, image, title, description }: Props) => {
   const cardRef = useRef<HTMLAnchorElement>(null)
-  const maskRef = useRef<HTMLDivElement>(null)
+  const layerRef = useRef<HTMLDivElement>(null)
   const imageRef = useRef<HTMLImageElement>(null)
 
   useGSAP(() => {
-    gsap.set(maskRef.current, {
+    gsap.set(layerRef.current, {
       opacity: 1,
-      yPercent: 100,
-      rotateX: 45,
+      z: "15vw",
+      rotationX: 36,
     })
 
     gsap.set(imageRef.current, {
       scale: 1.5,
-      filter: "blur(20px)",
+      filter: "blur(8px)",
     })
 
     const timeline = gsap.timeline({
       scrollTrigger: {
+        scrub: true,
         trigger: cardRef.current,
-        toggleActions: "play resume resume reset",
+        // toggleActions: "play resume resume reset",
         start: "top bottom",
+        end: "bottom top",
+        onEnter: () => {
+          gsap.to(imageRef.current, {
+            scale: 1,
+            filter: "blur(0px)",
+            ease: "expo.out",
+            duration: 1.5,
+          })
+        },
+        onEnterBack: () => {
+          gsap.to(imageRef.current, {
+            scale: 1,
+            filter: "blur(0px)",
+            ease: "expo.out",
+            duration: 1.5,
+          })
+        },
+        onLeave: () => {
+          gsap.to(imageRef.current, {
+            scale: 1.5,
+            filter: "blur(8px)",
+            ease: "expo.out",
+            duration: 1.5,
+          })
+        },
+        onLeaveBack: () => {
+          gsap.to(imageRef.current, {
+            scale: 1.5,
+            filter: "blur(8px)",
+            ease: "expo.out",
+            duration: 1.5,
+          })
+        },
       },
     })
 
     timeline.to(
-      maskRef.current,
+      layerRef.current,
       {
-        yPercent: 0,
-        rotateX: 0,
-        duration: 1.5,
-        ease: "expo.out",
+        z: "0vw",
+        rotationX: 0,
+        duration: 1,
+        ease: "circ.out",
       },
       0,
     )
 
     timeline.to(
-      imageRef.current,
+      layerRef.current,
       {
-        scale: 1,
-        filter: "blur(0px)",
-        ease: "expo.out",
-        duration: 1.5,
+        z: "15vw",
+        rotationX: -36,
+        duration: 1,
+        ease: "circ.in",
       },
-      0,
+      1,
     )
   })
 
   return (
     <Link ref={cardRef} href={`/work/${slug}`} className={`${styles.card} ${styles[align]} ${className}`}>
-      <div ref={maskRef} className={styles.mask}>
-        <div ref={imageRef} className={styles.image}>
-          {image}
+      <div ref={layerRef} className="layer">
+        <div className={styles.mask}>
+          <div ref={imageRef} className={styles.image}>
+            {image}
+          </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <h2>
-          <MaskHeading delay={0.125} reset={true}>
-            {title}
-          </MaskHeading>
-        </h2>
-        <p>
-          <ScrambleText duration={0.5} delay={0.5} reset={true}>
-            {description}
-          </ScrambleText>
-        </p>
+        <div className={styles.content}>
+          <h2>
+            <MaskHeading delay={0.125} reset={true}>
+              {title}
+            </MaskHeading>
+          </h2>
+          <p>
+            <ScrambleText duration={0.5} delay={0.5} reset={true}>
+              {description}
+            </ScrambleText>
+          </p>
+        </div>
       </div>
     </Link>
   )
