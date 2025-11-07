fecf44466f52f4530975b76e8df7b799e113f7e3
Joe di Stefano <joeydi@okaypl.us>
2025-11-07 16:04:59 -0500

    FeaturedProjects tailwind

--- app/components/FeaturedProjects.tsx
+++ app/components/FeaturedProjects.tsx
@@ -1,36 +1,26 @@
-import styles from "./FeaturedProjects.module.scss"
 import Container from "@/components/Container"
 import SplitHeading from "@/components/SplitHeading"
 import ProjectCard from "@/components/ProjectCard"
 import Row from "./Row"
 import Column from "./Column"
 import { getProjects } from "@/(admin)/admin/projects/actions"
-import Image from "next/image"
 
 const FeaturedProjects = async () => {
   const { projects } = await getProjects(1, 4)
 
   return (
-    <section className={styles.section}>
+    <section className="my-(--spacing-xl) perspective-[100vw]">
       <Container>
-        <h1 className={styles.heading}>
+        <h1 className="my-(--spacing-lg) w-[60%] min-w-fit">
           <SplitHeading>
             Featured <br /> Projects
           </SplitHeading>
         </h1>
         <Row>
           {projects.map((project, i) => {
-            const image = <Image fill src={project.thumbnailUrl || ""} alt={project.thumbnailAlt || ""} />
-
             return (
               <Column sm="6" key={`column-${i}`}>
-                <ProjectCard
-                  align={i % 2 ? "right" : "left"}
-                  slug={project.slug}
-                  image={image}
-                  title={project.title}
-                  description={project.shortDescription}
-                />
+                <ProjectCard project={project} align={i % 2 ? "right" : "left"} />
               </Column>
             )
           })}
