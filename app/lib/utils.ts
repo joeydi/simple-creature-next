cf8e64c6ea0fcd22c31a51df8f3987a6ad9d7827
Joe di Stefano <joeydi@okaypl.us>
2025-11-07 15:26:42 -0500

    format

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -6,25 +6,25 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function convertRange(value: number, r1: number[], r2: number[]) {
-  return ((value - r1[0]) * (r2[1] - r2[0])) / (r1[1] - r1[0]) + r2[0];
+  return ((value - r1[0]) * (r2[1] - r2[0])) / (r1[1] - r1[0]) + r2[0]
 }
 
 export function precisionRound(number: number, precision: number) {
-  const factor = Math.pow(10, precision);
-  return Math.round(number * factor) / factor;
+  const factor = Math.pow(10, precision)
+  return Math.round(number * factor) / factor
 }
 
 export function getRandom(min: number, max: number) {
-  return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min
 }
 
 export function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 export const randomHexColor = () => {
-  const n = (Math.random() * 0xfffff * 1000000).toString(16);
-  return "#" + n.slice(0, 6);
-};
+  const n = (Math.random() * 0xfffff * 1000000).toString(16)
+  return "#" + n.slice(0, 6)
+}
