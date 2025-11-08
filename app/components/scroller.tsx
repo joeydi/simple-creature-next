553d48edc33aab5341002b625737f1ff2fff1ff7
Joe di Stefano <joeydi@okaypl.us>
2025-11-07 19:25:48 -0500

    shift scroller over -x

--- app/components/scroller.tsx
+++ app/components/scroller.tsx
@@ -10,7 +10,7 @@ const Scroller = ({ children }: React.ComponentProps<"div">) => {
     <div
       className={cn(
         "ease-[cubic-bezier(0.08,0.94,0.26,0.95)] h-full overflow-auto bg-white transition duration-500",
-        isActive ? "scale-75 rounded-xl" : "scale-100 rounded-none",
+        isActive ? "-translate-x-[6.125%] scale-75 rounded-xl" : "scale-100 rounded-none",
       )}
     >
       {children}
