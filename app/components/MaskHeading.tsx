a2e25f2bc859f818889c2bcfaa65c4c6dbb8d33e
Joe di Stefano <joeydi@okaypl.us>
2026-05-12 14:49:40 -0400

    Fix MaskHeading

--- app/components/MaskHeading.tsx
+++ app/components/MaskHeading.tsx
@@ -1,70 +1,63 @@
-"use client";
+"use client"
 
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/SplitText";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef } from "react";
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { SplitText } from "gsap/SplitText"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useRef } from "react"
 
-gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger)
 
 interface Props {
-  delay?: number;
-  reset?: boolean;
+  delay?: number
+  reset?: boolean
 }
 
 const MaskHeading = ({ delay = 0, reset = false, children }: React.PropsWithChildren<Props>) => {
-  const parentRef = useRef<HTMLDivElement>(null);
-  const childRef = useRef<HTMLDivElement>(null);
+  const parentRef = useRef<HTMLDivElement>(null)
+  const childRef = useRef<HTMLDivElement>(null)
 
   useGSAP(() => {
-    const split = new SplitText(childRef.current, { type: "words" });
+    const split = new SplitText(childRef.current, { type: "words" })
+
+    gsap.set(split.words, { yPercent: 100, x: 100 })
+    gsap.set(parentRef.current, { opacity: 1 })
 
     const timeline = gsap.timeline({
       scrollTrigger: {
         trigger: parentRef.current,
         toggleActions: `play resume resume ${reset ? "reset" : "resume"}`,
       },
-    });
+    })
 
-    timeline.fromTo(
+    timeline.to(
       split.words,
-      {
-        yPercent: 100,
-      },
       {
         yPercent: 0,
         duration: 1,
         ease: "power3.out",
         stagger: 0.1,
       },
-      0 + delay
-    );
+      0 + delay,
+    )
 
-    timeline.fromTo(
+    timeline.to(
       split.words,
-      {
-        x: 100,
-      },
       {
         x: 0,
         duration: 1,
         ease: "power3.inOut",
         stagger: 0.1,
       },
-      0 + delay
-    );
-
-    gsap.set(parentRef.current, {
-      opacity: 1,
-    });
-  });
+      0 + delay,
+    )
+  })
 
   return (
     <div ref={parentRef} style={{ opacity: 0, overflow: "hidden" }}>
       <div ref={childRef}>{children}</div>
     </div>
-  );
-};
+  )
+}
 
-export default MaskHeading;
+export default MaskHeading
