c1684085458458cea95059ecdcf6bb14c03d13f8
Joe di Stefano <joeydi@okaypl.us>
2026-05-14 00:09:02 -0400

    remove contact link from menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -26,10 +26,6 @@ const links = [
     title: "About",
     url: "/about",
   },
-  {
-    title: "Contact",
-    url: "/contact",
-  },
 ]
 
 const Header = () => {
@@ -142,9 +138,12 @@ const Header = () => {
   const translations = ["-translate-y-12", "-translate-y-24", "-translate-y-36", "-translate-y-48", "-translate-y-60"]
 
   return (
-    <header className="fixed z-10 flex h-[clamp(100px,84px+5vw,180px)] w-full items-center pointer-events-none" ref={headerRef}>
+    <header
+      className="pointer-events-none fixed z-10 flex h-[clamp(100px,84px+5vw,180px)] w-full items-center"
+      ref={headerRef}
+    >
       <Container className="flex justify-end">
-        <div className="relative pointer-events-auto">
+        <div className="pointer-events-auto relative">
           <button className={cn(styles.menuButton, isActive ? styles.menuButtonActive : "")} onClick={clickHandler}>
             <span>Menu</span>
           </button>
