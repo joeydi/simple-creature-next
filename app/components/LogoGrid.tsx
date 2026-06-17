d885ed02b67f726e11477f9814aec16cf5d651ba
Joe di Stefano <joeydi@okaypl.us>
2026-06-17 14:18:25 -0400

    no relative on grid containers

--- app/components/LogoGrid.tsx
+++ app/components/LogoGrid.tsx
@@ -147,10 +147,10 @@ export const LogoGrid = () => {
   }, [randomLogos])
 
   return (
-    <section className="relative bg-black">
+    <section className="bg-black">
       <div
         ref={maskRef}
-        className="perspective-[100vw] mask-[linear-gradient(to_bottom,transparent_0px,black_300px,black_100%)] relative aspect-[1] w-full overflow-hidden lg:aspect-[1.5]"
+        className="perspective-[100vw] mask-[linear-gradient(to_bottom,transparent_0px,black_300px,black_100%)] aspect-[1] w-full overflow-hidden lg:aspect-[1.5]"
       >
         {/* <div className="bg-linear-[to_bottom,rgba(0,0,0,1),transparent_300px,transparent_calc(100%-100px),rgba(0,0,0,0.5)] z-1 pointer-events-none absolute inset-0"></div> */}
         <div ref={gridRef} className="transform-3d size-[200%]">
